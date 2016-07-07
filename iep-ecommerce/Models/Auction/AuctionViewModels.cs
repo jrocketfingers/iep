a015724e6e45a306f57ecde3ad70e52342b48d14
Johnny Rocketfingers <developer94@gmail.com>
2016-07-07 19:52:44 +0200

    Add display information to auction view models.

--- iep-ecommerce/Models/Auction/AuctionViewModels.cs
+++ iep-ecommerce/Models/Auction/AuctionViewModels.cs
@@ -11,9 +11,13 @@ namespace iep_ecommerce.Models.Auction
         [Display(Name = "Title")]
         public string Title { get; set; }
 
+        [Display(Name = "Lower Price Bound")]
         public double LowerBound { get; set; }
+
+        [Display(Name = "Upper Price Bound")]
         public double UpperBound { get; set; }
 
+        [Display(Name = "Auction State")]
         public Auction.State State { get; set; }
     }
 }
